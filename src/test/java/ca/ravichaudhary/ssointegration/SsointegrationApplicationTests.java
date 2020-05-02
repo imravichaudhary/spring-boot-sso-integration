24d9a94bd3ddf4251796143ddf5898d312e6ffc4
Ravi Chaudhary <ravi7131@yahoo.com>
2020-05-02 19:18:29 -0400

    Initial commit

---
+++ src/test/java/ca/ravichaudhary/ssointegration/SsointegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.ravichaudhary.ssointegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsointegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
