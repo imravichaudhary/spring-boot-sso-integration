ae7675df3d813f2a12048a767889ea31181049e6
Ravi Chaudhary <ravi7131@yahoo.com>
2020-05-02 20:02:55 -0400

    Adding saml dependency

--- src/main/java/ca/ravichaudhary/ssointegration/SSOIntegrationApplication.java
+++ src/main/java/ca/ravichaudhary/ssointegration/SSOIntegrationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SsointegrationApplication {
+public class SSOIntegrationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SsointegrationApplication.class, args);
+        SpringApplication.run(SSOIntegrationApplication.class, args);
     }
 
 }
---
+++ src/main/java/ca/ravichaudhary/ssointegration/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package ca.ravichaudhary.ssointegration.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .antMatchers("/pub/**").permitAll()
+                .antMatchers("/sec/**").authenticated()
+                .anyRequest().authenticated();
+    }
+}
